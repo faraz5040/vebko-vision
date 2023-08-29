2da456fe967a40b5445c2994514468c5cc2f88b7
Nima <88092252+Nima-Nilchian@users.noreply.github.com>
2023-08-29 08:28:37 +0330

    Add files via upload
    solved the merging dataframes challenge

--- mqtt.py
+++ mqtt.py
@@ -23,15 +23,19 @@ pos_df = pd.DataFrame()
 data_df = pd.DataFrame()
 
 def grouping_frame_tag(dataframe: pd.DataFrame):
-    pos_frame_grouped = dataframe.groupby('superFrameNumber')
-    pos_groups = pos_frame_grouped.groups
+    try:
+        pos_frame_grouped = dataframe.groupby('superFrameNumber')
+        pos_groups = pos_frame_grouped.groups
 
-    pos_frame_tag = []
-    for frame, indexes in pos_groups.items():
-        for ind in indexes:
-            pos_frame_tag.append(str(frame) + dataframe['Tag'].iloc[ind][-2:])
+        pos_frame_tag = np.zeros(len(dataframe), dtype='object')
+        for frame, indexes in pos_groups.items():
+            for ind in indexes:
+                pos_frame_tag[ind] = (str(frame) + dataframe['Tag'].iloc[ind][-2:])
 
-    dataframe['frame_tag'] = pos_frame_tag
+        dataframe['frame_tag'] = pos_frame_tag
+    except Exception as err:
+        print("Exception in grouping. Error:", err)
+
     return dataframe
 
 def main():
@@ -44,19 +48,23 @@ def main():
 
     while True:
         if keyboard.is_pressed('esc'):
-
             pos_df = grouping_frame_tag(pos_df)
             data_df = grouping_frame_tag(data_df)
 
             log = pd.merge(data_df, pos_df)
             log['Time (ms)'] = log.groupby('Tag')['Time (ms)'].transform(lambda x: x - x.min()) / 1000
+
             log.drop(columns=['superFrameNumber'], inplace=True)
+            log.drop(columns=['frame_tag'], inplace=True)
+
+            log = log.drop(log.columns[log.isna().sum() / len(log) > 0.9], axis=1)
+            log.dropna(inplace=True)
 
             log.rename(columns={'x': 'X', 'y': 'Y', 'z': 'Z', 'quality': 'Quality'}, inplace=True)
             log.index = log.index + 1
 
             num_tag = log['Tag'].nunique()
-            num_anch = log['Number of Anchors'].iloc[0]
+            num_anch = log['Number of Anchors'].iloc[1]
             rand_num = random.randint(1, 100000)
 
             file_name = f'log_{num_tag}_{num_anch}_{rand_num}.xlsx'
@@ -99,31 +107,63 @@ def subscribe(client: mqtt_client.Client):
         node = msg.topic[9:13]
 
         if "position" in payload:
-            pos = payload["position"]
+            pos = {}
+            pos['Tag'] = node
+
+            pos.update(payload["position"])
             pos['superFrameNumber'] = payload['superFrameNumber']
             for dim in ('x', 'y', 'z'):
                 pos[dim] = round(float(pos[dim]), 2)
-            pos['Tag'] = node
+
+            new_frame_num = pos['superFrameNumber']
+            try:
+                num_tags = pos_df['Tag'].nunique()
+                while new_frame_num in pos_df.superFrameNumber.values:
+                    if len(pos_df[pos_df.superFrameNumber == new_frame_num]['Tag'].values) == num_tags:
+                        new_frame_num += 2000
+                    else:
+                        break
+
+                pos['superFrameNumber'] = new_frame_num
+            except Exception as error:
+                print(f'Error in position {new_frame_num}, {error}')
 
             pos_df = pd.concat([pos_df, pd.DataFrame([pos])], ignore_index=True)
 
         elif "data" in payload:
-            bytes = base64.b64decode(payload["data"])
-            count = bytes[0]
-            timestamp = int.from_bytes(bytes[1:5], "little")
-            bytes = bytes[5:]
-            data = {
-                hex(
-                    int.from_bytes(bytes[6 * i: 2 + 6 * i], "little")
-                ): f'{int.from_bytes(bytes[2 + 6 * i: 6 * (i + 1)], "little")}mm'
-                for i in range(count)
-            }
-            print(f"node: {msg.topic[9:13]}, count: {count}, {data}, time (ms): {timestamp}")
-            data_dict = {'superFrameNumber': payload['superFrameNumber'],
-                         'Tag': node, 'Number of Anchors': count, 'Time (ms)': timestamp}
-            data_dict.update(data)
+            try:
+                bytes = base64.b64decode(payload["data"])
+                count = bytes[0]
+                timestamp = int.from_bytes(bytes[1:5], "little")
+                bytes = bytes[5:]
+                data = {
+                    hex(
+                        int.from_bytes(bytes[6 * i: 2 + 6 * i], "little")
+                    ): f'{int.from_bytes(bytes[2 + 6 * i: 6 * (i + 1)], "little")}mm'
+                    for i in range(count)
+                }
+                data_dict = {'superFrameNumber': payload['superFrameNumber'],
+                             'Tag': node, 'Number of Anchors': count, 'Time (ms)': timestamp}
+                data_dict.update(data)
 
-            data_df = pd.concat([data_df, pd.DataFrame([data_dict])], ignore_index=True)
+                new_frame_num = data_dict['superFrameNumber']
+                try:
+                    num_tags = data_df['Tag'].nunique()
+                    while new_frame_num in data_df.superFrameNumber.values:
+                        if len(data_df[data_df.superFrameNumber == new_frame_num]['Tag'].values) == num_tags:
+                            new_frame_num += 2000
+                        else:
+                            break
+
+                    data_dict['superFrameNumber'] = new_frame_num
+                except Exception as error:
+                    print(f'Error in data {new_frame_num}, {error}')
+
+                data_df = pd.concat([data_df, pd.DataFrame([data_dict])], ignore_index=True)
+                print(f"node: {msg.topic[9:13]}, count: {count}, {data}, time (ms): {timestamp}, row: {len(data_df)}")
+
+            except Exception as err:
+                print("Error in data", err)
 
         else:
             print(f"Received `{payload}` from `{msg.topic}` topic")
@@ -132,7 +172,6 @@ def subscribe(client: mqtt_client.Client):
     client.subscribe(topic)
     # set the on_message callback, receives the topic every time its been sent
     client.on_message = on_message
-
 
 if __name__ == "__main__":
     main()
